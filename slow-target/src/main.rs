db7234cdd6223bf2a24e3bc1a99a40f12a169062
Miro Svrtan <miro@mirosvrtan.me>
2019-07-01 20:48:15 +0200

    Build simple slow target website
    It returns the provided id, together with information when request was
    created and responded.
    
    For now it will sleep for 5 seconds, this seems to be enough just for
    the testing purposes.

--- slow-target/src/main.rs
+++ slow-target/src/main.rs
@@ -1,12 +1,44 @@
 #![feature(proc_macro_hygiene, decl_macro)]
 
-#[macro_use] extern crate rocket;
+#[macro_use]
+extern crate rocket;
+extern crate chrono;
 
-#[get("/")]
-fn index() -> &'static str {
-    "Hello, world!"
+use chrono::{DateTime, SecondsFormat, Utc};
+use rocket_contrib::json::Json;
+use serde::Serialize;
+use std::{thread, time};
+
+#[get("/user/<id>")]
+fn index(id: i64) -> Json<ApiResult> {
+    let requested_at = current_time();
+
+    let sleep_for = time::Duration::from_secs(5);
+    thread::sleep(sleep_for);
+
+    let responded_at = current_time();
+
+    let result = ApiResult {
+        id: id,
+        requested_at: requested_at,
+        responded_at: responded_at,
+    };
+
+    Json(result)
 }
 
 fn main() {
     rocket::ignite().mount("/", routes![index]).launch();
+}
+
+fn current_time() -> String {
+    let now: DateTime<Utc> = Utc::now();
+    now.to_rfc3339_opts(SecondsFormat::Secs, true)
+}
+
+#[derive(Debug, Serialize)]
+struct ApiResult {
+    id: i64,
+    requested_at: String,
+    responded_at: String,
 }
