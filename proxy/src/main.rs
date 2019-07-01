5fb134efafab3b66aef90eaddbddfb6d6dbc1ec2
Miro Svrtan <miro@mirosvrtan.me>
2019-07-01 23:20:30 +0200

    wip: locking based on ID implemented

--- proxy/src/main.rs
+++ proxy/src/main.rs
@@ -11,37 +11,66 @@ use serde::Deserialize;
 use serde::Serialize;
 use std::clone::Clone;
 use std::collections::HashMap;
+use std::collections::HashSet;
 use std::sync::{Arc, RwLock};
+use std::{thread, time};
 
+type MyCache = Arc<RwLock<Cache>>;
+type MyProcessing = Arc<RwLock<Processing>>;
 #[get("/user/<id>")]
-fn index(repo: State<Arc<RwLock<Repository>>>, id: i64) -> Json<ProxyResult> {
-    let lookedup = repo.clone().read().unwrap().lookup(&id);
+fn index(cache: State<MyCache>, grab: State<MyProcessing>, id: i64) -> Json<ProxyResult> {
+    try_cached_response_or_fetch_it(cache, grab, id)
+}
+
+fn try_cached_response_or_fetch_it(
+    cache: State<MyCache>,
+    grab: State<MyProcessing>,
+    id: i64,
+) -> Json<ProxyResult> {
+    let lookedup = cache.clone().read().unwrap().lookup(&id);
 
     //
 
     match lookedup {
-        Some(data) => {
-            let response = ProxyResult::Ok(data.clone(), true);
-            Json(response)
-        }
+        Some(data) => Json(ProxyResult::Cached(data.clone())),
         None => {
-            let result = ask(id);
-            match result {
-                Ok(data) => {
-                    let mut lock = repo.write().unwrap();
-                    let api_result: ApiResult = serde_json::from_str(data.as_str()).unwrap();
+            let being_processed = grab.clone().read().unwrap().in_processing(&id);
 
-                    lock.store(&api_result);
+            println!("In processing:{}", being_processed);
 
-                    let response = ProxyResult::Ok(api_result, false);
-                    Json(response)
+            match being_processed {
+                true => {
+                    println!("Sleeping:{}", &id);
+                    thread::sleep(time::Duration::from_millis(50));
+
+                    try_cached_response_or_fetch_it(cache, grab, id)
                 }
-                Err(_) => {
-                    let response = ProxyResult::Err();
-                    Json(response)
-                }
+                false => fetch_it(cache, grab, id),
             }
         }
+    }
+}
+fn fetch_it(cache: State<MyCache>, grab: State<MyProcessing>, id: i64) -> Json<ProxyResult> {
+    println!("Fetching data:{}", &id);
+
+    println!("Locking grab:{}", &id);
+    grab.write().unwrap().lock(&id);
+    println!("Locked grab:{}", &id);
+
+    let result = ask(id);
+    match result {
+        Ok(data) => {
+            let api_result: ApiResult = serde_json::from_str(data.as_str()).unwrap();
+
+            cache.write().unwrap().store(&api_result);
+
+            println!("Unlocking grab:{}", &id);
+            grab.write().unwrap().unlock(&id);
+            println!("Unlocked grab:{}", &id);
+
+            Json(ProxyResult::Ok(api_result))
+        }
+        Err(_) => Json(ProxyResult::Err()),
     }
 }
 
@@ -62,7 +91,8 @@ fn ask(id: i64) -> Result<String, Box<dyn std::error::Error>> {
 
 fn main() {
     rocket::ignite()
-        .manage(Arc::new(RwLock::new(Repository::new())))
+        .manage(Arc::new(RwLock::new(Cache::new())))
+        .manage(Arc::new(RwLock::new(Processing::new())))
         .mount("/", routes![index, homepage])
         .launch();
 }
@@ -76,17 +106,18 @@ pub struct ApiResult {
 
 #[derive(Debug, Serialize, Deserialize)]
 enum ProxyResult {
-    Ok(ApiResult, bool),
+    Cached(ApiResult),
+    Ok(ApiResult),
     Err(),
 }
 
-pub struct Repository {
+pub struct Cache {
     data: HashMap<i64, ApiResult>,
 }
 
-impl Repository {
-    pub fn new() -> Repository {
-        Repository {
+impl Cache {
+    pub fn new() -> Cache {
+        Cache {
             data: HashMap::new(),
         }
     }
@@ -101,6 +132,33 @@ impl Repository {
         match self.data.get(id) {
             Some(x) => Some(x.clone()),
             None => None,
+        }
+    }
+}
+
+pub struct Processing {
+    data: HashSet<i64>,
+}
+
+impl Processing {
+    pub fn new() -> Processing {
+        Processing {
+            data: HashSet::new(),
+        }
+    }
+
+    pub fn lock(&mut self, id: &i64) -> bool {
+        self.data.insert(id.clone())
+    }
+
+    pub fn unlock(&mut self, id: &i64) -> bool {
+        self.data.remove(id)
+    }
+
+    pub fn in_processing(&self, id: &i64) -> bool {
+        match self.data.get(id) {
+            Some(_) => true,
+            None => false,
         }
     }
 }
